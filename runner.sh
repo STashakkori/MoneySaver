aae9f4b0570823c276d039e7e4ddde31f851df6e
Sina Tashakkori <sinatashakkori@gmail.com>
2023-09-12 15:52:42 -0400

    Update runner.sh

--- runner.sh
+++ runner.sh
@@ -1,10 +1,8 @@
 #!/bin/bash
 
-# Check for the correct number of command-line arguments
 if [ $# -ne 4 ]; then
     echo "Usage: ./run.sh <directory> <file_extensions> <line_number> <text_to_insert>"
     exit 1
 fi
 
-# Run the Kotlin program
 kotlin InsertTextKt "$1" "$2" "$3" "$4"
